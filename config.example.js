60f2394ac9f2ec5b12f0aad3abcad880d04bb397
Jet Lee <pupboss.i@gmail.com>
2016-11-06 14:04:01 +0800

    improved

--- config.example.js
+++ config.example.js
@@ -7,8 +7,9 @@ var config = {
   port: 3001, // 本地端口号
   first_week_monday: '2016-08-29T00:00:00.000+08:00', // 每学期的第一个周一，每学期都需要手动改一次
   server_name: 'Aliyun-Node', // 服务器名字，选填
-  secret_key: 'wv6GC3iMpUJa', // 加盐
+  secret_key: 'wv6GC3iMpUJa', // 加盐，必须修改，稍微复杂点不能太长
   auto_send: false, // 自动发送
+
   db: {
     uri: 'mongodb://name:pass@localhost:27017/db' // 数据库地址
   },
