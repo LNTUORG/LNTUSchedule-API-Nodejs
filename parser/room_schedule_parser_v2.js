1c1bad85b0bebbaf4b13e69f8607aa4eb7a08476
Jet Lee <pupboss.i@gmail.com>
2018-01-10 10:28:39 +0800

    Updated

--- parser/room_schedule_parser_v2.js
+++ parser/room_schedule_parser_v2.js
@@ -16,37 +16,18 @@ var analyse_room = function(aid, buildingid, whichweek, week, callback) {
       if (err) {
         return callback(constant.cookie.net_error, null);
       }
-      var temp_dict = {};
-      for (var k = 0; k < res.body.results.length; k++) {
-        temp_dict[res.body.results[k].name] = res.body.results[k].status;
-      }
-      var dict_arr = [];
 
-      model.building_model.find({ location_id: aid, building_id: buildingid }, function (error, docs) {
-        if (error || docs.length == 0) {
-          dict_arr = res.body.results;
+      var dict = {};
+      dict.results = res.body.results;
+      dict.week = whichweek;
+      dict.weekday = week;
+      model.system_config_model.find({ key: constant.config_key.first_week_monday }, function (error, docs) {
+        if(error || docs.length < 1){
+          return callback(constant.cookie.args_error, null);
         } else {
-          var rooms = docs[0]['rooms'];
-          for (var l = 0; l < rooms.length; l++) {
-            if (!(rooms[l] in temp_dict)) {
-              dict_arr.push({name: rooms[l], status: ['1', '1', '1', '1', '1']});
-            } else {
-              dict_arr.push({name: rooms[l], status: temp_dict[rooms[l]]});
-            }
-          }
+          dict.first_week_monday_at = docs[0].value;
+          return callback(null, dict);
         }
-        var dict = {};
-        dict.results = dict_arr;
-        dict.week = whichweek;
-        dict.weekday = week;
-        model.system_config_model.find({ key: constant.config_key.first_week_monday }, function (error, docs) {
-          if(error || docs.length < 1){
-            return callback(constant.cookie.args_error, null);
-          } else {
-            dict.first_week_monday_at = docs[0].value;
-            return callback(null, dict);
-          }
-        });
       });
     });
 };
