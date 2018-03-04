e8d7afd7ba41ebf11924ae47eba38bb67f6b0681
Jet Lee <pupboss.i@gmail.com>
2018-03-04 14:30:17 +0800

    Sort the classes by name

--- parser/room_schedule_parser_v2.js
+++ parser/room_schedule_parser_v2.js
@@ -7,6 +7,26 @@ var constant = require('../agent/constant');
 var model = require('../utility/db');
 var superagent = require('superagent');
 
+var by = function(name){
+  return function(o, p){
+    var a, b;
+    if (typeof o === "object" && typeof p === "object" && o && p) {
+      a = o[name];
+      b = p[name];
+      if (a === b) {
+        return 0;
+      }
+      if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+      }
+      return typeof a < typeof b ? -1 : 1;
+    }
+    else {
+      throw ("error");
+    }
+  }
+}
+
 var analyse_room = function(aid, buildingid, whichweek, week, callback) {
 
   superagent
@@ -19,6 +39,7 @@ var analyse_room = function(aid, buildingid, whichweek, week, callback) {
 
       var dict = {};
       dict.results = res.body.results;
+      dict.results.sort(by("name"));
       dict.week = whichweek;
       dict.weekday = week;
       model.system_config_model.find({ key: constant.config_key.first_week_monday }, function (error, docs) {
--- test/superagent.js
+++ test/superagent.js
@@ -1,13 +1,37 @@
 var superagent = require('superagent');
 
 superagent
-  .get('https://api.lgdjwc.com/v1/room-schedule?location_id=1&building_id=10&week=4&week_day=4')
+  .get('https://api.lgdjwc.com/v1/room-schedule?location_id=3&building_id=6&week=1&week_day=1')
   .timeout(3000000)
   .end(function(err, res) {
     if (err) {
     	console.log(err);
     }
-    var result = res.body;
+    var result = res.body.results;
+
+
+    result.sort(by("name"));
     console.log(result);
     // return callback(null, result);
   });
+
+
+var by = function(name){
+  return function(o, p){
+    var a, b;
+    if (typeof o === "object" && typeof p === "object" && o && p) {
+      a = o[name];
+      b = p[name];
+      if (a === b) {
+        return 0;
+      }
+      if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+      }
+      return typeof a < typeof b ? -1 : 1;
+    }
+    else {
+      throw ("error");
+    }
+  }
+}
